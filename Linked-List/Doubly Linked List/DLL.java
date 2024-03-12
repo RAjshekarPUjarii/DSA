7f7eeb3e73a5550fb9743f575bb9dce01123a1de
RajshekarPujari0 <rajshekarpujari2002@gmail.com>
2024-03-12 18:04:53 +0530

    Add Doubly linked list Problems

--- Linked-List/Doubly Linked List/DLL.java
+++ Linked-List/Doubly Linked List/DLL.java
@@ -30,6 +30,7 @@ public class DLL {
     }
 // convert array to doubly linked list
     public static Node convertArr2DLL(int[] arr){
+        if(arr.length == 0)return null;
         Node head = new Node(arr[0],null,null);
         Node prev = head;
         for(int i =1;i<arr.length; i++){
@@ -65,13 +66,139 @@ public class DLL {
         return head;
         
     }
+
+    // delete at the Kth position of the doubly linked list
+    public static Node deleteAtKthPos(Node head, int k){
+
+        if(head == null)return null;
+
+
+        Node temp = head;
+        int count = 0;
+        while(temp!=null ){
+            count++;
+            if(count == k ){
+                break;
+            }
+            temp = temp.next;
+            
+        }
+        
+        
+       
+        Node prev = temp.back;
+        Node front = temp.next;
+        
+        if(front == null && prev == null){
+            return null;
+        }else if(front == null){
+            return deleteAtTail(head);
+        }else if(prev == null){
+            return deleteAtHead(head);
+        }
+
+        prev.next = front;
+        front.back = prev;
+        temp.next = null;
+        temp.back = null;
+
+        return head;
+        
+    }
+
+    // delete the node with the given value
+    private static void deleteNode(Node temp){
+         Node prev = temp.back;
+        Node front = temp.next;
+        if(front==null){
+            prev.next = null;
+            temp.back = null;
+            return;
+        }
+        
+       
+        prev.next = front;
+        front.back = prev;
+        
+        temp.next = null;
+        temp.back = null;
+        
+       
+        return;
+}
+
+//   Insertion problems
+// 1. Insertion at the head
+public static Node insertAtHead(Node head, int data){
+
+    if(head == null){
+        return new Node(data);
+    }
+    Node temp = new Node(data);
+    temp.next = head;
+    head.back = temp;
+    return temp;
+}
+
+// 2. Insertion at the tail
+public static Node insertAtTail(Node head, int data){
+    if(head == null){
+        return new Node(data);
+    }
+    Node temp = head;
+    while(temp.next!=null){
+        temp = temp.next;
+    }
+    Node newNode = new Node(data);
+    temp.next = newNode;
+    newNode.back = temp;
+    newNode.next = null;
+    return head;
+}
+
+//  insert before the tail
+public static Node insertBeforeTail(Node head, int data){
+    if(head == null){
+        return new Node(data);
+    }
+   Node newNode = new Node(data);
+    if(head.next == null){
+   newNode.next = head;
+   newNode.back = null;
+   head.back = newNode;
+}
+
+Node temp =head;
+while(temp.next!= null){
+    temp = temp.next;
+}
+Node back = temp.back;
+
+back.next = newNode;
+newNode.back = back;
+
+newNode.next = temp;
+temp.back = newNode;
+
+
+
+return head;
+}
+
    
     public static void main(String[] args) {
     
-        int arr[] = {1,2,3,4};
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
         Node head =  convertArr2DLL(arr);
+        // head = deleteAtHead(head)
+        // head = deleteAtTail(head);
+        //   head = deleteAtKthPos(h ead,9);
+        //  deleteNode(head.next);
         
-        head = deleteAtTail(head);
+        // insertions
+        // head = insertAtHead(head,990);
+        // head = insertAtTail(head,757);
+        head = insertBeforeTail(head, 100);
        
         display(head);
 
